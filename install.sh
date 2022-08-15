9b2521e03b33dcba7dc7cbe4c679075ab4413856
Sabi GuJJar <99659601+SapreetGujjar@users.noreply.github.com>
2022-08-15 18:41:43 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#installation
+
+clear
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+toilet -f future "Sapreet GuJJar" | lolcat
+echo "-------------------------------------------------------" | lolcat
+echo   "Created  : Sabi GuJJar $white" |lolcat
+echo "-------------------------------------------------------" | lolcat
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit  ... "
+echo ""
+sleep 1
+echo ""
+echo "Sabi GuJJar" | lolcat
+sleep 1
+exit
+}
+
+apt update
+apt upgrade -y
+pkg install ruby -y 
+gem install toilet -y 
+pkg install lolcat
+pkg install figlet
+apt install git php openssh curl -y
+pkg install python2 -y 
+pkg install nano php -y
+pip2 install mechanize
+pip2 install --upgrade pip
+
+echo ""
+echo "CODE_BY_Sabi_GuJJar" | lolcat
+echo "________________________" | lolcat
+sleep 10
